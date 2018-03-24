b777c6eda245b17e9e710ee6025c23b6829f5ff0
elmcgill <37718257+elmcgill@users.noreply.github.com>
2018-03-24 18:10:27 -0500

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,6 +1,9 @@
 package wizards.main;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable{
 
@@ -9,7 +12,8 @@ public class Game extends Canvas implements Runnable{
 	 */
 	private static final long serialVersionUID = -4984271202747625627L;
 	public final int WIDTH = 1280, HEIGHT = WIDTH/12*9;
-	
+	private Thread thread;
+	private boolean running = false;
 	
 	public Game() {
 		new Window(WIDTH, HEIGHT, "Wizards Wonds", this);
@@ -18,13 +22,75 @@ public class Game extends Canvas implements Runnable{
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
+		long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(running)
+        {
+                    long now = System.nanoTime();
+                    delta += (now - lastTime) / ns;
+                    lastTime = now;
+                    while(delta >=1)
+                            {
+                                tick();
+                                delta--;
+                            }
+                            if(running)
+                                render();
+                            frames++;
+                            
+                            if(System.currentTimeMillis() - timer > 1000)
+                            {
+                                timer += 1000;
+                                System.out.println("FPS: "+ frames);
+                                frames = 0;
+                            }
+        }
+                stop();
+		
+	}
+	
+	private void tick() {
+		
+		
+	}
+	
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs==null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.BLACK);
+		g.fillRect(0,0,WIDTH,HEIGHT);
+		//g.setColor(Color.WHITE);
+		//g.fillRect(100, 300, 50, 100);
+		
+		g.dispose();
+		bs.show();
 		
 	}
 	
 	public synchronized void start() {
-		
-		
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running = false;
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 		
 	}
 	
