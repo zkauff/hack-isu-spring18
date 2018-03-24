7296b51232103b79971763b77ae58103ad713009
tfuchs98 <tfuchs@iastate.edu>
2018-03-23 21:18:25 -0500

    Update Projectile.java

--- Projectile.java
+++ Projectile.java
@@ -1,5 +1,28 @@
 package wizard_wars;
 
-public class Projectile {
+	private int damage;
+	private int speed;
+  private int areaOfEffect;	
 
+	public Projectiles(int damage, int speed, int aoe) {
+		this.damage = damage;
+		this.speed = speed;
+    this.areaOfEffect = aoe;
+	}
+	
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	
+	public int getDamage() {
+		return damage;
+	}
+
+  public void setSpeed(int speed){
+    this.speed = speed;
+  }
+  
+  public int getSpeed(){
+    
+  }
 }
